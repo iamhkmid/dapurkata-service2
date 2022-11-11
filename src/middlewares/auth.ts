11d8a11583db0bd063b6716b47c21d93568f92dc
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2022-11-11 14:25:06 +0700

    update auth mdw

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,24 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { httpCatchError } from "../helpers/httpCatchError";
 import { prisma } from "../prismaClient";
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.headers["authorization"];
-    if (!token) throw new Error(JSON.stringify({ statusCode: "401", message: "Token not found" }))
+    if (!token) throw new Error("Token not found")
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
     const findUser = await prisma.user.findUnique({
       where: { username: (<any>decoded)["username"] }
     });
-    if (!findUser) throw new Error(JSON.stringify({ statusCode: "401", message: "User not found" }))
+    if (!findUser) throw new Error("User not found")
     next()
   } catch (error: any) {
-    if (error?.message)
-      res.status(401).send({
-        statusCode: "401",
-        message: error?.message
-      })
-    else httpCatchError({ error, res })
+    res.status(401).send({
+      statusCode: "401",
+      message: error?.message
+    })
   }
 }
