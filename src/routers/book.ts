5d79d20a1a102e5ea6c258e2cb208a3dae2b2689
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2022-11-12 20:12:11 +0700

    fix prisma

--- src/routers/book.ts
+++ src/routers/book.ts
@@ -3,25 +3,23 @@ import { httpCatchError } from "../helpers/httpCatchError";
 import { saveFile } from "../helpers/saveFile";
 import { addBookSchema, deleteBookSchema, updateBookSchema } from "../helpers/validationSchemas";
 import { auth } from "../middlewares/auth";
-import fs from "fs"
-import { promisify } from "util"
-import { db } from "..";
 import { prisma } from "../prismaClient";
+import fs from "fs"
 
 const BookRouter = express.Router();
 
 BookRouter.use("/get", async (req, res) => {
-  try {
-    db.connect();
-    const query = promisify(db.query).bind(db);
-    const findBook = await query('SELECT * FROM books') as Promise<[]>
-    res.send({ statusCode: 200, data: findBook })
-  } catch (error) {
-    throw error
+  const bookId = req.body.bookId
+  if (bookId) {
+    const findBook = await prisma.book.findUnique({ where: { id: bookId } });
+    res.send({ statusCode: "200", data: findBook })
+  } else {
+    const findBook = await prisma.book.findMany()
+    res.send({ statusCode: "200", data: findBook })
   }
 });
 
-BookRouter.use("/add", async (req, res) => {
+BookRouter.use("/add", auth, async (req, res) => {
   try {
     await addBookSchema.validate(req.body);
     const { pathFile } = await saveFile({ dir: "/public/uploads/books", file: req.body.cover, name: `${new Date().getTime()}-${req.body?.title}`, limit: 1048576 })
@@ -36,7 +34,7 @@ BookRouter.use("/add", async (req, res) => {
         publisher: req.body.publisher || undefined
       }
     })
-    res.send({ status: "200", data: addBook }).end()
+    res.json({ status: "200", data: addBook })
   } catch (error) {
     httpCatchError({ error, res })
   }
@@ -47,7 +45,7 @@ BookRouter.use("/delete", auth, async (req, res) => {
     await deleteBookSchema.validate(req.body);
     const deleteBook = await prisma.book.delete({ where: { id: req.body?.bookId } })
     if (deleteBook?.coverUrl) fs.unlinkSync(`${process.cwd()}/public${deleteBook?.coverUrl}`);
-    res.send({ status: "200", data: deleteBook })
+    res.json({ status: "200", data: deleteBook })
   } catch (error) {
     httpCatchError({ error, res })
   }
@@ -74,7 +72,7 @@ BookRouter.use("/update", auth, async (req, res) => {
         publisher: req.body.publisher || undefined
       }
     })
-    res.send({ status: "200", data: updateBook })
+    res.json({ status: "200", data: updateBook })
   } catch (error) {
     httpCatchError({ error, res })
   }
