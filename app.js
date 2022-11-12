c53b99ea1be336116e04765eb45bb6ca35c8b707
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2022-11-12 17:51:19 +0700

    add main

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+  
+// Routes
+  
+app.get('/', function(req, res) {
+res.send('Hello World! Ini adalah Website Express.js pertama saya');
+ });
+  
+// Listen
+  
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
